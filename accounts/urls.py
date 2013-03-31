574e6eb5a1770c337b0036c2820947ca6c489efd
Jack Twilley <mathuin@gmail.com>
2013-03-31 16:32:43 -0700

    urls.defaults is deprecated, use urls instead.
    Replaced urls.defaults with urls since the former is now deprecated.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns
 
 urlpatterns = patterns('accounts.views',
                        (r'^register/$', 'register',
