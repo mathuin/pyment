265585b95fcf33846a696214c2fdc3150b113242
Jack Twilley <mathuin@gmail.com>
2013-03-31 21:32:35 -0700

    Added new command 'crate_transfer' and added dry run options.  Fixed #16.
    Added the new command to support moving all the jars in one crate to
    another crate.  Also added 'dry run' options to several other
    management commands.
    
    The crate_transfer command uses numerical arguments for crates.  That
    makes sense.  I changed add_new_jars to use numerical arguments too.

--- inventory/management/commands/add_new_jars.py
+++ inventory/management/commands/add_new_jars.py
@@ -1,10 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
 from inventory.models import Jar, Crate
 from catalog.models import Product
+from optparse import make_option
 
 class Command(BaseCommand):
-    args = '<product> <start_jar> <end_jar> <crate>'
+    args = '<product> <start_jar> <end_jar> <crate number>'
     help = 'Creates new jars in a particular product placing them in a particular crate'
+    option_list = BaseCommand.option_list + (
+        make_option('--dry-run', 
+                    action='store_true', 
+                    dest='dryrun', 
+                    default=False, 
+                    help='Simulate the command'),
+        )
 
     def handle(self, *args, **options):
         if len(args) != 4:
@@ -30,16 +38,19 @@ class Command(BaseCommand):
             raise CommandError, 'Start jar value must be less than or equal to end jar value'
         if Jar.objects.filter(product=product, number__in=jar_list).exists():
             raise CommandError, 'Jars already exist in this product within this range'
-        crate_name = args[3]
-        _, cratenumber = crate_name.split(' ', 1)
-        if Crate.objects.filter(number=cratenumber).exists():
-            crate = Crate.objects.get(number=cratenumber)
+        try:
+            crate_number = args[3]
+        except ValueError:
+            raise CommandError('Crate is not an int: %s' % args[3])
+        if Crate.objects.filter(number=crate_number).exists():
+            crate = Crate.objects.get(number=crate_number)
         else:
-            raise CommandError, 'Not a valid crate: %s' % crate_name
+            raise CommandError, 'Not a valid crate: %d' % crate_number
         if (crate.jars + len(jar_list)) > crate.capacity:
             raise CommandError, 'Crate capacity would be exceeded: %d > %d' % (crate.jars + len(jar_list), crate.capacity)
         # actually do the work
         for jar_number in jar_list:
-            jar = Jar(product=product, number=jar_number, crate=crate, is_active=True, is_available=True)
-            jar.save()
-        self.stdout.write('%s jars were created in %s and placed in %s\n' % (len(jar_list), product, crate))
+            if not options['dryrun']:
+                jar = Jar(product=product, number=jar_number, crate=crate, is_active=True, is_available=True)
+                jar.save()
+        self.stdout.write('%d jars were created in %s and placed in %s\n' % (len(jar_list), product, crate))
---
+++ inventory/management/commands/crate_transfer.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand, CommandError
+from inventory.models import Jar, Crate
+from catalog.models import Product
+from optparse import make_option
+
+class Command(BaseCommand):
+    args = '<source number> <dest number>'
+    help = 'Move jars from one crate to another.'
+    option_list = BaseCommand.option_list + (
+        make_option('--dry-run', 
+                    action='store_true', 
+                    dest='dryrun', 
+                    default=False, 
+                    help='Simulate the command'),
+        )
+
+    def handle(self, *args, **options):
+        if len(args) != 2:
+            raise CommandError('Wrong number of arguments')
+        try:
+            source_number = args[0]
+        except ValueError:
+            raise CommandError('Source crate is not an int: %s' % args[0])
+        if Crate.objects.filter(number=source_number).exists():
+            source = Crate.objects.get(number=source_number)
+        else:
+            raise CommandError('Source crate not valid: %d' % source_number)
+        try:
+            dest_number = args[1]
+        except ValueError:
+            raise ValueError('Destination crate is not an int: %s' % args[1])
+        if Crate.objects.filter(number=dest_number).exists():
+            dest = Crate.objects.get(number=dest_number)
+        else:
+            raise CommandError('Destination crate not valid: %d' % dest_number)
+        if source.jars + dest.jars > dest.capacity:
+            raise CommandError('Destination crate does not have enough room')
+        if not Jar.objects.filter(crate=source).exists():
+            raise CommandError('Source crate is empty')
+        source_jars = Jar.objects.filter(crate=source)
+        if not options['dryrun']:
+            for jar in source_jars:
+                jar.crate = dest
+                jar.save()
+        self.stdout.write('%d jars were moved from crate %d to crate %d' % (len(source_jars), source_number, dest_number))
