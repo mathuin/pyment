4b2a6c7eb2f40fd6a004e82d8d837604bb93d2d9
Jack Twilley <mathuin@gmail.com>
2013-04-07 22:43:35 -0700

    Added link from product jars to inventory by product.  Fixes #20.
    I've added the cross-link from product to inventory.

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,7 +8,7 @@ from django.utils.safestring import mark_safe
 class ProductAdmin(admin.ModelAdmin):
     form = ProductAdminForm
     # sets values for how the admin site lists your products
-    list_display = ('name', 'title', 'category', 'description', 'jars_in_stock', 'is_active', 'created_at', 'updated_at',)
+    list_display = ('name', 'title', 'category', 'description', 'link_jars', 'is_active', 'created_at', 'updated_at',)
     list_display_links = ('name',)
     list_filter = ('is_active', 'category')
     # list_per_page = 50
@@ -19,13 +19,22 @@ class ProductAdmin(admin.ModelAdmin):
     # totally lame, must do the hard way
     # prepopulated_fields = {'slug' : ('brewname', 'batchletter',)}
 
+    def link_jars(self, obj):
+        howmany = obj.jars_in_stock()
+        anchor = '%s?product__id__exact=%d' % (reverse('admin:inventory_jar_changelist'), obj.pk)
+        if howmany > 0:
+            return mark_safe('%d (<a href="%s">list</a>)' % (howmany, anchor))
+        else:
+            return howmany
+    link_jars.short_description = 'Jars'
+
 # registers your product model with the admin site
 admin.site.register(Product, ProductAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     #sets up values for how admin site lists categories
-    list_display = ('name', 'bjcptag', 'count', 'is_active', 'created_at', 'updated_at',)
+    list_display = ('name', 'bjcptag', 'link_products', 'is_active', 'created_at', 'updated_at',)
     list_display_links = ('name',)
     list_filter = ('is_active',)
     list_per_page = 20
@@ -35,7 +44,7 @@ class CategoryAdmin(admin.ModelAdmin):
     # sets up slug to be generated from category name
     prepopulated_fields = {'slug': ('name', )}
 
-    def count(self, obj):
+    def link_products(self, obj):
         howmany = obj.products.count()
         # FIXME: learn how to create the URL for 'the admin page for products filtering on this particular category'
         # http://localhost:8000/admin/catalog/product/?category__id__exact=1
@@ -44,7 +53,7 @@ class CategoryAdmin(admin.ModelAdmin):
             return mark_safe('%d (<a href="%s">list</a>)' % (howmany, anchor))
         else:
             return howmany
-    count.short_description = 'Products'
+    link_products.short_description = 'Products'
 
 admin.site.register(Category, CategoryAdmin)
 
