d9cba2e1f7b7fb82862a4eb7ee5f423ade36f823
Jack Twilley <mathuin@gmail.com>
2015-05-19 19:03:38 -0700

    Updated integration test to use docker-compose.
    Out with the fig!

--- integration-test.sh
+++ integration-test.sh
@@ -8,15 +8,15 @@
 # integrated as fixtures.  For now, real data works!
 
 # import fixtures
-#fig run web python manage.py syncdb --noinput  # <-- once fixtures are ready
-fig start db && \
+#docker-compose run web python manage.py syncdb --noinput  # <-- once fixtures are ready
+docker-compose start db && \
     docker run --link pyment_db_1:pyment_db_1 aanand/wait && \
     docker exec -i pyment_db_1 psql -U pyment < pyment_db.bak && \
-    fig stop db
+    docker-compose stop db
 # to *export* fixtures, do this:
-# fig start db && \
+# docker-compose start db && \
 #     sleep 15 && \
 #     docker exec -i pyment_db_1 pg_dump -U pyment pyment > newpyment.bak && \
-#     fig stop db
+#     docker-compose stop db
 # populate media directory
-fig run web tar -C /opt/public -xf /opt/app/media.tar
+docker-compose run web tar -C /opt/public -xf /opt/app/media.tar
