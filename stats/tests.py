089bc8be6283b2ad1b54e9e643b081510ac0a15c
Jack Twilley <mathuin@gmail.com>
2015-05-26 21:36:13 -0700

    Updated test files with notes.
    These notes will help me figure out which tests to write.

--- stats/tests.py
+++ stats/tests.py
@@ -1,16 +1,12 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
 
+# stats.py
+# recommended_from_search
+# (probably best solved in search/tests.py)
+# recommended_from_views
+# (hopefully just comes up after viewing)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
--- utils/tests.py
+++ utils/tests.py
@@ -1,16 +1,10 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
 
+# modeladmin.py
+# SmarterModelAdmin
+# not sure if it's relevant.
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
