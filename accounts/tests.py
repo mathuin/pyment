a7217a8a5f8ade737a00648e1955305a55efc62d
Jack Twilley <mathuin@gmail.com>
2015-05-28 00:10:36 -0700

    First steps with accounts.
    Created a test for the register view for accounts.

--- accounts/tests.py
+++ accounts/tests.py
@@ -13,3 +13,13 @@ from django.core.urlresolvers import reverse
 # test my_account (just get)
 # test order_details
 # test order_info (get and post)
+
+
+class RegisterTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('register')
+
+    def test_register(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Sign Up For Your Account')
