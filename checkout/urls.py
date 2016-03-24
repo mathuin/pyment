356255325f3e384a60f45f532ddfb280eb955915
Jack Twilley <mathuin@gmail.com>
2016-03-23 18:01:42 -0700

    Used the wrong names on checkout urls
    Oops.  Fixed it.

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from views import show_checkout, receipt
 
 urlpatterns = [
-    url(r'^$', show_checkout, {'template_name': 'checkout/checkout.djhtml'}, name='show_checkout'),
-    url(r'^receipt/$', receipt, {'template_name': 'checkout/receipt.djhtml'}, name='receipt'),
+    url(r'^$', show_checkout, {'template_name': 'checkout/checkout.djhtml'}, name='checkout'),
+    url(r'^receipt/$', receipt, {'template_name': 'checkout/receipt.djhtml'}, name='checkout_receipt'),
 ]
