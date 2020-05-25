9198a5ed8e13869169d1659f3178484e9a1c200d
Jack Twilley <mathuin@gmail.com>
2020-05-24 18:26:39 -0700

    Fix the SSL thing again

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
+set -x
 
 # clean up current situation
 docker-compose down -v
 
-# patch devssl certificate
-sudo chown 70 devssl/server.key
-
 # rebuild containers
 docker-compose build
 
-# populate public directory and nginx directory
+# populate public directory and nginx directory and development SSL directory
 docker run --rm -it -v pyment_public:/public -v $(pwd):/backup busybox tar -C /public -xf /backup/public.tar
 docker run --rm -it -v pyment_nginx:/nginx -v $(pwd):/backup busybox cp /backup/nginx/pyment.conf /nginx
+docker run --rm -it -v pyment_devssl:/devssl -v $(pwd):/backup busybox tar -C /devssl -xf /backup/devssl.tar
 
 # collectstatic
 docker-compose run --rm web python manage.py collectstatic --noinput
 
 # create database
 docker-compose run --rm web python manage.py migrate
+
+# output logs if error
+if [[ $? == 1 ]]; then
+  docker logs pyment_db_1
+fi
 
 # load fixtures
 apps="accounts checkout meadery inventory"
--- devssl/README.md
+++
@@ -1,8 +0,0 @@
-# devssl
-
-These files are for development and CI purposes only.
-They are not for use in production.
-
-https://gist.github.com/mrw34/c97bb03ea1054afb551886ffc8b63c3b is where I got the script to generate these.
-
-Note that the file `server.key` must be owned by uid 70.  This is done via sudo in the dev setup script.
