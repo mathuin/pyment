c4bc01e725ccc48afee00ea005703e65b4f7ced2
Jack Twilley <mathuin@users.noreply.github.com>
2022-11-28 16:32:46 -0800

    Use correct settings location (#143)

--- django/pyment/wsgi.py
+++ django/pyment/wsgi.py
@@ -16,7 +16,7 @@ framework.
 from django.core.wsgi import get_wsgi_application
 import os
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyment.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
